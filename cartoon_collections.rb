b1f77497c5ea432af58b81b8f18ebb6dc2f81ef6
w.h.sinn <w.h.sinn@gmail.com>
2019-03-21 20:41:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,43 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves=%w["Doc", "Dopey", "Bashful", "Grumpy"]
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
+end
+roll_call_dwarves(dwarves)
+
+
+
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect {|call| call.capitalize + "!"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+
+
+def long_planeteer_calls(calls_long)
+  index = 0
+  while index < calls_long.length do
+    current_call = calls_long[index]
+    if current_call.length > 4
+    return true
+    end
+    index += 1
+  end
+      return false
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(contains_cheddar)
+  while index < contains_cheddar.length do
+    current_snack = contains_cheddar[index]
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.include?("current_snack")
+  if true 
+    return current_snack
+    index += 1
+end
+nil
 end
